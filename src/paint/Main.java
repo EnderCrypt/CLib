f175594fec0f183dd5fcacc71b4da0876211ae11
Magnus <EnderCrypt@gmail.com>
2015-10-04 22:13:34 +0200

    made some enhancements to the paint demo (the selected color now blinks)

--- src/paint/Main.java
+++ src/paint/Main.java
@@ -2,8 +2,11 @@ package paint;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Point;
 import java.io.File;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.github.EnderCrypt.CLib.CLib;
 import com.github.EnderCrypt.CLib.event.CLibMouseData;
@@ -12,6 +15,9 @@ import com.github.EnderCrypt.CLib.event.CLibMouselistener;
 public class Main
 	{
 	public static char icon = (char) 219;
+	public static char halfIcon = (char) 219-(16*2)-10;
+	public static boolean blink = true;
+	public static Point selected = new Point(0,0);
 	public static char lineIcon = (char) 196;
 	public static int colorHeight = 2;
 	public static Dimension screenSize = new Dimension(50, 25);
@@ -56,6 +62,7 @@ public class Main
 			{
 			clib.put(x, screenSize.height-colorHeight-1, lineIcon);
 			}
+		clib.setFrontBrush(Color.BLACK);
 		// add listeners
 		clib.addMouseListener(new CLibMouselistener()
 			{
@@ -69,9 +76,9 @@ public class Main
 				{
 				if (e.tile.y >= (screenSize.height-colorHeight))
 					{
-					int x = e.tile.x;
-					int y = e.tile.y-screenSize.height+colorHeight;
-					Color color = colorBar[y][x];
+					selected.x = e.tile.x;
+					selected.y = e.tile.y-screenSize.height+colorHeight;
+					Color color = colorBar[selected.y][selected.x];
 					clib.setFrontBrush(color);
 					}
 				}
@@ -90,5 +97,25 @@ public class Main
 					}
 				}
 			});
+		// blinking
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask()
+			{
+			@Override
+			public void run()
+				{
+				blink = !blink;
+				int y = screenSize.height-colorHeight+selected.y;
+				if (blink)
+					{
+					clib.put(selected.x, y, icon);
+					}
+				else
+					{
+					clib.put(selected.x, y, halfIcon);
+					}
+				clib.redraw();
+				}
+			}, 500,500);
 		}
 	}
